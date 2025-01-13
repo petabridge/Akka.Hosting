440fb89d3dd27cf2f3be265daafc80b89e16a6b4
Gregorius Soedharmo <arkatufus@yahoo.com>
2025-01-14 04:07:14 +0700

    Bump MS.EXT, BCL, and Json to 8.0.X (#553)
    * Bump MS.EXT, BCL, and Json to 8.0.X
    
    * Update API approval list

--- src/Akka.Hosting.TestKit/Internals/XUnitLogger.cs
+++ src/Akka.Hosting.TestKit/Internals/XUnitLogger.cs
@@ -68,7 +68,7 @@ namespace Akka.Hosting.TestKit.Internals
             };
         }
 
-        public IDisposable BeginScope<TState>(TState state) 
+        public IDisposable BeginScope<TState>(TState state) where TState : notnull 
         {
             throw new NotImplementedException();
         }
